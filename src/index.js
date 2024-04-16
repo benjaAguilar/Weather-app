331da19b691c3dafec00a4bc6501dec19ef29426
benjaAguilar <benjaaguilar02@gmail.com>
2024-04-16 15:53:56 -0300

    added the distribution file

--- src/index.js
+++ src/index.js
@@ -6,6 +6,23 @@ import getCoords from './userLocation';
 import getWeather from './getWeather';
 import { switchLoading } from './printWeather';
 
+// icons
+import arrow from './imgs/arrow.svg';
+import logo from './imgs/logo.svg';
+import search from './imgs/search.svg';
+import fLike from './imgs/feels-like.svg';
+import humidity from './imgs/humidity.svg';
+import wind from './imgs/wind.svg';
+import gif from './imgs/logo-gif.gif';
+
+document.querySelector('.logo-img').src = logo;
+document.querySelector('.search-icon').src = search;
+document.querySelector('.flike').src = fLike;
+document.querySelector('.humidi').src = humidity;
+document.querySelector('.windi').src = wind;
+document.querySelector('.arrow').src = arrow;
+document.querySelector('.gifaso').src = gif;
+
 const searchBar = document.querySelector('#searchbar');
 const homeBtn = document.querySelector('.my-home');
 
